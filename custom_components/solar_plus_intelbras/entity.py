d19aa30a1a47ecb49116cc3b478c854411675906
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2025-02-10 22:18:16 -0300

    feat: add entity for device

--- custom_components/solar_plus_intelbras/entity.py
+++ custom_components/solar_plus_intelbras/entity.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from .const import ATTRIBUTION
+from .const import ATTRIBUTION, DOMAIN
 from .coordinator import SolarPlusIntelbrasDataUpdateCoordinator
 
 
@@ -21,10 +21,12 @@ class SolarPlusIntelbrasEntity(
         super().__init__(coordinator)
         self._attr_unique_id = coordinator.config_entry.entry_id
         self._attr_device_info = DeviceInfo(
-            identifiers={
-                (
-                    coordinator.config_entry.domain,
-                    coordinator.config_entry.entry_id,
-                ),
-            },
+            identifiers={(DOMAIN, coordinator.config_entry.entry_id)},
+            name="Solar Plus Intelbras",
+            manufacturer="Intelbras",
+            model="Solar Plus",
+            sw_version="1.0.0",
         )
+
+
+    
